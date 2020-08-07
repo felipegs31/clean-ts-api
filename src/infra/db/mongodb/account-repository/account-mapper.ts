3919cf5013b4a2e2441b3ef8d9f9cf5b91b741ac
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2020-08-07 00:09:40 -0300

    refactor: move mapping to mongo helper

---
+++ src/infra/db/mongodb/account-repository/account-mapper.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from './../../../../domain/models/account'
+export const map = (account: any): AccountModel => {
+  const { _id, ...accountWithoutId } = account
+  return { ...accountWithoutId, id: _id }
+}
