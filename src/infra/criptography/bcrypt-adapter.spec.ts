4ef76fec00a806dc1921fd369cd127d653b5b214
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2020-08-06 22:49:24 -0300

    feat: ensure bcryupt adapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,12 +1,25 @@
 import { BcryptAdapter } from './bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash_string'))
+  }
+}))
+
 describe('Bcrypt adapter', () => {
-  test('should cann bcrypt with corret value', async () => {
+  test('should call bcrypt with corret value', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash_string')
   })
 })
