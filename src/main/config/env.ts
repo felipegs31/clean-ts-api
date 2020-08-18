c9f0ad8801fa7e516f3a124f40a24ac8e6a4f01f
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2020-08-18 09:55:30 -0300

    feat: connect to mongodb

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://felipegs31:159753feh@nodecourse.bvjjv.mongodb.net/manguinho?retryWrites=true&w=majority',
+  port: process.env.MONGO_URL || 5050
+}
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,8 +5,5 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   console.log('pre route')
-  fg.sync('**/src/main/routes/**routes.ts').forEach(async file => {
-    console.log('file', file)
-    return (await import(`../../../${file}`)).default(router)
-  })
+  fg.sync('**/src/main/routes/**routes.ts').forEach(async file => (await import(`../../../${file}`)).default(router))
 }
