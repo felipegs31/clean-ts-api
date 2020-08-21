12e05b027f01176e57309cf1389d9ff1d4a2a8a9
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2020-08-21 01:00:33 -0300

    refactor: move sut to a factory

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,22 +2,40 @@ import { HttpRequest, HttpResponse } from './../../presentation/protocols/http'
 import { Controller } from './../../presentation/protocols/controller'
 import { LogControllerDecorator } from './log'
 
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeCotroller = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Rodrigo'
+        }
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeCotroller()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'Rodrigo'
-          }
-        }
-        return new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
+
     const httpRequest = {
       body: {
         email: 'anywmail@mail.com',
@@ -26,6 +44,7 @@ describe('LogController Decorator', () => {
         passwordConfirmation: 'anypassword'
       }
     }
+
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
