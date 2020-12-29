d0612745c7c99002555c680d0176e09659b0b7a9
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2020-12-28 22:45:43 -0300

    refactor: build docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://felipegs31:IcNMHhtFyKaVijlU@nodecourse.bvjjv.mongodb.net/manguinho?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://felipegs31:IcNMHhtFyKaVijlU@nodecourse.bvjjv.mongodb.net/manguinho?retryWrites=true&w=majority', // nao vou mudar pro container
   port: process.env.MONGO_URL || 5050,
   jwtSecret: process.env.JWT_SECRET || 'FA937A9iasud012'
 }
